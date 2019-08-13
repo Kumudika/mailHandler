3086c50936e77f3759a360222aea966952e30a88
janithal <janithal@codegen.net>
2019-08-13 11:59:10 +0530

    refactor 2

--- src/main/java/client/ClientMailSender.java
+++ src/main/java/client/ClientMailSender.java
@@ -1,7 +1,7 @@
 package client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import model.SendEmailAck;
 import model.SendEmailReq;
 
 import java.io.*;
@@ -10,47 +10,57 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Random;
 
-public class ClientMailSender implements Runnable {
-    private InetAddress host;
-    ObjectMapper objectMapper;
-    private int port;
-    SendEmailReq mailRequest;
-    private String requestId;
-    Random random;
-    private  int rr;
+public class ClientMailSender implements Runnable
+{
+	private InetAddress host;
+	private int port;
+	SendEmailReq sendEmailReq;
+	private String requestId;
+	Random random;
+	private int rr;
+	private Gson gson = new Gson();
 
-    public ClientMailSender( SendEmailReq mailRequest) {
-        try {
-            objectMapper = new ObjectMapper();
-            host = InetAddress.getLocalHost();
-            port = 2500;
-            this.mailRequest = mailRequest;
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
-    public void run() {
-        try(
-                Socket socket = new Socket(host, port);
-                PrintWriter out =
-                        new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()))
-        ){
-            String writeJson = objectMapper.writeValueAsString(mailRequest);
-            out.println(writeJson);
+	public ClientMailSender( SendEmailReq sendEmailReq )
+	{
+		try
+		{
+			host = InetAddress.getLocalHost();
+			port = 2500;
+			this.sendEmailReq = sendEmailReq;
+		}
+		catch ( UnknownHostException e )
+		{
+			e.printStackTrace();
+		}
+	}
 
-            String readValue = in.readLine();
+	public void run()
+	{
+		try (
+				Socket socket = new Socket( host, port );
+				PrintWriter out =
+						new PrintWriter( socket.getOutputStream(), true );
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader( socket.getInputStream() ) )
+		)
+		{
+			String writeJson = gson.toJson( sendEmailReq, SendEmailReq.class );
+			out.println( writeJson );
+			String readValue = in.readLine();
+			SendEmailAck resMsg = gson.fromJson( readValue, SendEmailAck.class );
+			System.out.println(resMsg);
 
-            int sleepTime = random.nextInt(450) + 50;
-            Thread.sleep(sleepTime);
-        } catch(IOException e)
-        {
-            e.printStackTrace();
-        } catch(InterruptedException e)
-        {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        }
-    }
+			int sleepTime = random.nextInt( 450 ) + 50;
+			Thread.sleep( sleepTime );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+		catch ( InterruptedException e )
+		{
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+	}
 }
--- src/main/java/client/MailClient.java
+++ src/main/java/client/MailClient.java
@@ -5,26 +5,32 @@ import model.SendEmailReq;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MailClient {
-    public static void main(String[] args) {
-        while (args.length == 2) {
-            int requests = Integer.parseInt(args[0]);
-            int threads = Integer.parseInt(args[1]);
-            ExecutorService executorService = Executors.newFixedThreadPool(threads);
+public class MailClient
+{
+	public static void main( String[] args )
+	{
+		if ( args.length == 2 )
+		{
+			int requests = Integer.parseInt( args[0] );
+			int threads = Integer.parseInt( args[1] );
+			ExecutorService executorService = Executors.newFixedThreadPool( threads );
 
-            for (int i = 0; i < requests; i++) {
-                executorService.execute( new ClientMailSender(getRq(String.valueOf(i))) );
-            }
+			for ( int i = 0; i < requests; i++ )
+			{
+				executorService.execute( new ClientMailSender( getRq( String.valueOf( i ) ) ) );
+			}
 
-        }
-    }
-    public static SendEmailReq getRq(String requestId){
-        SendEmailReq emailReqMsg = new SendEmailReq(  );
-        emailReqMsg.setRequestId( requestId);
-        emailReqMsg.setBody( "Body" );
-        emailReqMsg.setReceipentEmail( "janithalokuge92@gmail.com" );
-        emailReqMsg.setSubject( requestId );
-        return  emailReqMsg;
-    }
+		}
+	}
+
+	public static SendEmailReq getRq( String requestId )
+	{
+		SendEmailReq emailReqMsg = new SendEmailReq();
+		emailReqMsg.setRequestId( requestId );
+		emailReqMsg.setBody( "Body" );
+		emailReqMsg.setReceipentEmail( "janithalokuge92@gmail.com" );
+		emailReqMsg.setSubject( requestId );
+		return emailReqMsg;
+	}
 
 }
