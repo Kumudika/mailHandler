3fa4073e1007c598a42ad022d0d09a72c9e44c3f
Kumudika Jayantha <kumudika.j@ustocktrade.com>
2019-08-13 11:21:22 +0530

    update 1.2

--- src/main/java/client/MailClient.java
+++ src/main/java/client/MailClient.java
@@ -1,18 +1,18 @@
 package client;
 
+import android.util.Log;
 import model.MailRequest;
-
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MailClient {
+    private static String TAG = "MailClient";
     public static void main(String[] args) {
         while (args.length == 2) {
             int requests = Integer.parseInt(args[0]);
             int threads = Integer.parseInt(args[1]);
             ExecutorService executorService = Executors.newFixedThreadPool(threads);
-
+            Log.i(TAG, "Initiate Mail Client");
             for (int i = 0; i < requests; i++) {
                 executorService.execute( new ClientMailSender(getRq(String.valueOf(i))) );
             }
